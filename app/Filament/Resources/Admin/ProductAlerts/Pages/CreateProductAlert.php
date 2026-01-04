ca3ca8854f80a134db297f389f094737e60f0947
Maximus <maduemeziachinedu6@gmail.com>
2026-01-04 16:34:43 +0100

    feat(CreateProductAlert): add user ID to created data on product alert creation

--- app/Filament/Resources/Admin/ProductAlerts/Pages/CreateProductAlert.php
+++ app/Filament/Resources/Admin/ProductAlerts/Pages/CreateProductAlert.php
@@ -2,10 +2,18 @@
 
 namespace App\Filament\Resources\Admin\ProductAlerts\Pages;
 
-use App\Filament\Resources\Admin\ProductAlerts\ProductAlertResource;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\Admin\ProductAlerts\ProductAlertResource;
 
 class CreateProductAlert extends CreateRecord
 {
     protected static string $resource = ProductAlertResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_by_user_id'] = Auth::id();
+        
+        return $data;
+    }
 }
